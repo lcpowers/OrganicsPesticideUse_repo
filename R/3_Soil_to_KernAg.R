9d0dcb0f6d6407065ef02f2fa3419609ebc62e58
lcpowers <lcpowers@ucsb.edu>
2020-06-12 08:52:28 -0600

    Trying to make soil work faster -- replaced rasterize with fasterize.

--- R/3_Soil_to_KernAg.R
+++ R/3_Soil_to_KernAg.R
@@ -4,12 +4,8 @@ Soil_to_KernAg_fun = function(year){
   timestamp()
   
   ### Read in raw Kern County Agriculture shapefile ###
-  ag = readOGR(paste0("../R_input/spatial/kern_AG_shp/kern",year,"/kern",year,".shp")) %>% 
-    spTransform(., 
-                CRS("+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs "))
-  
-  ## Change into simple features dataframe
-  ag_sf = st_as_sf(ag) %>% 
+  ag = read_sf(paste0("../R_input/spatial/kern_AG_shp/kern",year,"/kern",year,".shp")) %>% 
+    st_transform(.,CRS("+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")) %>% 
     filter(P_STATUS == "A" & !str_detect(COMM,"UNCULTIVATED")) %>%  # Keep active permits
     mutate(AG_TRS = paste0(TOWNSHIP,RANGE,str_pad(SECTION,2,pad="0",side=c("left")))) %>% 
     dplyr::select(PERMIT,PERMITTEE,PMT_SITE,COMM,S_STATUS,ACRES,AG_TRS) 
@@ -23,13 +19,13 @@ Soil_to_KernAg_fun = function(year){
   # ag_merge_check <- ag_merge %>% 
   #  as.data.frame(.) %>% 
   #  filter(is.na(FAMILY))
-  # 
+ 
   ag_slct <- ag_merge %>% 
     dplyr::select(-c(COMM_EDIT,COMM_CODEOLD))
   
   ## Extract values from soil raster that correspond to each polygon in the ag shapefile
   r.vals = raster::extract(soil_ras, # Storie Index Raster Values
-                           ag_sf, # Extract based on Kern Ag polygon outlines from the Kern Ag shapefile
+                           ag_slct, # Extract based on Kern Ag polygon outlines from the Kern Ag shapefile
                            fun = mean, # Find the mean value of raster cells that overlap with each polygon
                            small = TRUE, # Include mean values for small polygons
                            weights = TRUE, # Add weights for weighted means
